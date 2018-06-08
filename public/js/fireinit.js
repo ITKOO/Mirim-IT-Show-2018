b068d03006d2989863e98ec8672579004934c73c
ITKOO <j4550118@e-mirm.hs.kr>
2018-06-08 11:41:52 +0900

    웹사이트 시작 페이지 및 입력폼 프론트, 백완성(사진추가 포함X)

---
+++ public/js/fireinit.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBN20lxXffksSZKxCk71NdO5jnHhhyy6o8",
+    authDomain: "mirim-it-show-2018.firebaseapp.com",
+    databaseURL: "https://mirim-it-show-2018.firebaseio.com",
+    projectId: "mirim-it-show-2018",
+    storageBucket: "mirim-it-show-2018.appspot.com",
+    messagingSenderId: "594894209652"
+  };
+  firebase.initializeApp(config);
